2a2793576285b69fb3bd5452972e09143bbb3414
Dima S <dsuprunsw@gmail.com>
2023-05-29 03:06:38 -0700

    Fix typo.

--- gesture_handlers/build_handler.py
+++ gesture_handlers/build_handler.py
@@ -1,6 +1,5 @@
 from .GestureHandler import GestureHandler
 from .PyAutoGuiHandler import PyAutoGuiGestureHandler
-from .GestureHandler import GestureHandler
 
 def build_gesture_handler(handler_type, cooldownS) -> GestureHandler:
     if handler_type == 'arrows':
